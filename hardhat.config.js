e214f302a9fd267d916b9a0f4cd2a37e3998795a
levblanc <levblanc@gmail.com>
2022-11-04 17:55:59 +0800

    Update configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,58 +1,73 @@
-require("@nomicfoundation/hardhat-toolbox")
-require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const {
+    LOCAL_BLOCK_CONFIRMATIONS,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("./helper-hardhat-config");
+
+const { GOERLI_ALCHEMY_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.7",
-            },
-            {
-                version: "0.6.6",
-            },
-            {
-                version: "0.4.24",
+                version: "0.8.17",
             },
         ],
     },
+    defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
+            blockConfirmations: LOCAL_BLOCK_CONFIRMATIONS,
         },
         localhost: {
             chainId: 31337,
+            blockConfirmations: LOCAL_BLOCK_CONFIRMATIONS,
+            url: "http://127.0.0.1:8545",
         },
         goerli: {
-            url: GOERLI_RPC_URL,
             chainId: 5,
-        },
-        mainnet: {
-            url: MAINNET_RPC_URL,
-            chainId: 1,
+            url: GOERLI_ALCHEMY_URL,
+            blockConfirmations: VERIFICATION_BLOCK_CONFIRMATIONS,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
         },
     },
-    defaultNetwork: "hardhat",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
     etherscan: {
         // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-        apiKey:  ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
+        customChains: [
+            {
+                network: "goerli",
+                chainId: 5,
+                urls: {
+                    apiURL: "http://api-goerli.etherscan.io/api",
+                    browserURL: "https://goerli.etherscan.io",
+                },
+            },
+        ],
     },
     gasReporter: {
         enabled: false,
-        currency: "USD",
-        outputFile: "gas-report.txt",
+        outputFile: "gas-report.md",
         noColors: true,
     },
     contractSizer: {
         runOnCompile: false,
     },
     paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./build/cache",
-    artifacts: "./build/artifacts"
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./build/cache",
+        artifacts: "./build/artifacts",
     },
     mocha: {
         timeout: 200000, // 200 seconds max for running tests
     },
-}
+};
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -11,13 +11,15 @@ const networkConfig = {
     5: {
         name: "goerli",
     },
-}
+};
 
-const developmentChains = ["hardhat", "localhost"]
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const developmentChains = ["hardhat", "localhost"];
+const LOCAL_BLOCK_CONFIRMATIONS = 1;
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 module.exports = {
     networkConfig,
     developmentChains,
+    LOCAL_BLOCK_CONFIRMATIONS,
     VERIFICATION_BLOCK_CONFIRMATIONS,
-}
+};
